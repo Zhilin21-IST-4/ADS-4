352853bcc08e9c42632904a3eec2adf3660010e5
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-05-02 18:10:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count1 = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count1++;
       }
@@ -13,47 +13,47 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count2 = 0;
-  for (int i = 0; i < len - 1; i++) {
-    if (arr[i] <= value) {
-      for (int j = len - 1; j > i; j--) {
-        if (arr[i] + arr[j] == value) {
-          count++;
+  while (arr[len - 1] > value) {
+    len--;
+  }
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count2++;
         }
       }
     }
-  }
   return count2;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count3 = 0; 
+  int count3 = 0;
   for (int i = 0; i < len - 1; i++) {
     int left = i;
     int right = len;
-    while (left < right-1) {
+    while (left < right - 1) {
       int mid = (left + right) / 2;
       if (arr[i] + arr[mid] == value) {
-        count3++;
+        ++count3;
         int j = mid + 1;
         while (arr[i] + arr[j] == value && j < right) {
-          count3++;
-          j++;
+          ++count3;
+          ++j;
         }
         j = mid - 1;
         while (arr[i] + arr[j] == value && j > left) {
-          count3++;
-          count3--;
-          j--;
+          ++count3;
+          --j;
         }
         break;
       }
-      if (arr[i] + arr[mid] > value) {
-        right = mid;
-      }
-      else {
+      if (arr[i] + arr[mid] < value)
         left = mid;
-      }
+      if (arr[i] + arr[mid] > value)
+        right = mid;
     }
   }
-return count3;
+  if (count3 != 0)
+    return count3;
+  return 0;
 }
